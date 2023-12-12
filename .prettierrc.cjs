028c566252c0b57585e30077fe68441602481e4a
andresin87 <andresin87@gmail.com>
2023-12-12 12:08:30 +0100

    chore: missed some files

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5'
+}
+
+module.exports = config
